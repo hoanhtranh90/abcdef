3b0e60d65b9c350c325bf9ece1633232b49b418a
sangnk <nguyenkhacsang100@gmail.com>
2021-06-29 12:28:24 +0700

    f

---
+++ page1/script.js
@@ -0,0 +1,26 @@
+﻿//  $(function(){
+// 	  //hiệu ứng khi kích vào class nutmenu
+//  	 $('.icon').click(function() {
+//         $('.menu-left').addClass('ra');
+//         return false;
+//     });
+//     //dong menu
+//  	 $('.fa-times').click(function(event) {
+//         $('.menu-left').removeClass('ra');
+//         return false;
+//     });
+
+
+    
+// })  
+ 
+
+function changeClass(){
+    let menu = document.getElementsByClassName("menu-left")[0];
+    menu.classList.add("ra");
+}
+
+function closeMenu() {
+    let menu = document.getElementsByClassName("menu-left")[0];
+    menu.classList.remove("ra");
+}
